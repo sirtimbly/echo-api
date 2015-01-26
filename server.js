50852d42f9e43d9d087d19372ddde90e34dc033c
Tim Bendt <sirtimbly@gmail.com>
2015-01-25 23:43:27 -0700

    express for heroku to run me on root get

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ var myEcho = new Echo();
 //myEcho.apis.push(new HueApi());
 myEcho.apis.push(new PushApi());
 
-setInterval(function() {
-  myEcho.fetchTasks();
-}, 1500);
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+	setInterval(function() {
+	  myEcho.fetchTasks();
+	}, 1500);
+  response.send('Echo Listener Running');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
